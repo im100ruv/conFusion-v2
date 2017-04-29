d18774bb5fdad69bfe5d1bfeae53783c5b4ec693
im100ruv <karnasaurabh123@gmail.com>
2017-04-29 18:32:08 +0530

    Handling errors using $resource

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,10 +8,17 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = "Loading ...";
             
-            $scope.dishes = menuFactory.getDishes().query();
+            $scope.dishes = menuFactory.getDishes().query(
+            function(response) {
+                $scope.dishes = response;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: "+response.status + " " + response.statusText;
+            });
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -72,26 +79,34 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
         
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message="Loading ...";
             
-            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(
+                    function(response){
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    }
+            );
             
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
-            
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope,menuFactory) {
+            $scope.stars = [ 1, 2, 3, 4, 5 ];
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
             $scope.submitComment = function () {
                 
                 $scope.mycomment.date = new Date().toISOString();
                 console.log($scope.mycomment);
-                
                 $scope.dish.comments.push($scope.mycomment);
-                
+
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
                 $scope.commentForm.$setPristine();
-                
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             }
         }])
@@ -100,10 +115,19 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 
                 'corporateFactory', function($scope, menuFactory, corporateFactory) {
 
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message="Loading ...";
                     
-            $scope.dish = menuFactory.getDishes().get({id:0});
+            $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                    function(response){
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    }
+            );
                     
             var promotion= menuFactory.getPromotion(0);
             
